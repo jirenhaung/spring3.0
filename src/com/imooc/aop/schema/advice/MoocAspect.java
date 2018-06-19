9a7c58816b0f9834fc9913a6f197c06010e48af7
jirenhaung <1072425143@qq.com>
2018-06-19 19:57:00 +0800

    advice one

--- src/com/imooc/aop/schema/advice/MoocAspect.java
+++ src/com/imooc/aop/schema/advice/MoocAspect.java
@@ -1,5 +1,16 @@
 package com.imooc.aop.schema.advice;
 
 public class MoocAspect {
-
+	public void before(){
+		System.out.println("MoocAspect before");
+	}
+	public void afterReturning(){
+		System.out.println("MoocAspect afterReturning");
+	}
+	public void afterThrowing(){
+		System.out.println("MoocAspect afterThrowing");
+	}
+	public void after(){
+		System.out.println("MoocAspect after");
+	}
 }
--- src/com/imooc/aop/schema/advice/biz/AspectBiz.java
+++ src/com/imooc/aop/schema/advice/biz/AspectBiz.java
@@ -1,5 +1,9 @@
 package com.imooc.aop.schema.advice.biz;
 
-public class AspectBiz {
 
+public class AspectBiz {
+	public void biz(){
+		System.out.println("AspectBiz biz");
+		//throw new RuntimeException();
+	}
 }
